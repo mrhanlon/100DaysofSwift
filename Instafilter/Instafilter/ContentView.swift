f265f65531dccd5991abfb06de0dd09e4b228ac1
Matthew R Hanlon <mrhanlon@gmail.com>
2024-03-05 11:09:24 -0600

    Days 62-3

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Matthew Hanlon on 3/4/24.
+//
+
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import SwiftUI
+
+struct ContentView: View {
+    @State private var image: Image?
+
+    var body: some View {
+        VStack {
+            ContentUnavailableView {
+                Label("No snippets", systemImage: "swift")
+            } description: {
+                Text("You don't have any saved snippets yet.")
+            } actions: {
+                Button("Create Snippet") {
+                    // create a snippet
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Matthew Hanlon on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
