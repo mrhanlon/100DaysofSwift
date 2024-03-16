6fcd9d185235c9e1b111c59a95b64ecc45db57e4
Matthew R Hanlon <mrhanlon@gmail.com>
2024-03-16 13:27:24 -0500

    Day 84

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -12,6 +12,8 @@ struct MeView: View {
     @AppStorage("name") private var name = "Anonymous"
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
 
+    @State private var qrCode = UIImage()
+
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
 
@@ -24,16 +26,23 @@ struct MeView: View {
 
                 TextField("Email address", text: $emailAddress)
                     .textContentType(.emailAddress)
+                    .textInputAutocapitalization(.never)
                     .font(.title)
 
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
             }
             .navigationTitle("Your code")
         }
+        .onAppear(perform: updateCode)
+        .onChange(of: name, updateCode)
+        .onChange(of: emailAddress, updateCode)
     }
 
     func generateQRCode(from string: String) -> UIImage {
@@ -46,6 +55,10 @@ struct MeView: View {
         }
 
         return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
 }
 
