121e4ffff49c685ab7198284192693cbe83ca094
Matthew R Hanlon <mrhanlon@gmail.com>
2024-03-22 11:37:37 -0500

    Day 94

--- LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,24 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
-                    Text("Number \(num)")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(.red)
-                        .frame(width: 200, height: 200)
-                        .visualEffect { content, proxy in
-                            content
-                                .rotation3DEffect(.degrees(-proxy.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                        }
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(makeColor(proxy.frame(in: .global), fullView.frame(in: .global), index == 20))
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(proxy.frame(in: .global).minY / 200)
+                            .scaleEffect(scale(proxy.frame(in: .global), fullView.frame(in: .global), index == 20))
+                    }
+                    .frame(height: 40)
                 }
             }
-            .scrollTargetLayout()
         }
-        .scrollTargetBehavior(.viewAligned)
+    }
+
+    func scale(_ inner: CGRect, _ outer: CGRect, _ log: Bool) -> Double {
+        return (inner.minY / outer.maxY) + 0.5
+    }
+
+    func makeColor(_ inner: CGRect, _ outer: CGRect, _ log: Bool) -> Color {
+
+        if log {
+            print(max(0, min(1, inner.maxY / outer.maxY)))
+        }
+
+        return Color(hue: max(0, min(1, inner.maxY / outer.maxY)), saturation: 1.0, brightness: 1.0)
     }
 }
 
